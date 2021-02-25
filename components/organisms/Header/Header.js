59c2f7fbcd57fb6056dda4d4f2d32d92ebb10895
d-floutier <48097275+d-floutier@users.noreply.github.com>
2021-02-25 16:57:57 +0100

    Initial commit

---
+++ components/organisms/Header/Header.js
@@ -0,0 +1,50 @@
+import Container from '@/components/atoms/Container'
+import Logo from '@/components/atoms/Logo'
+import {MenuContext} from '@/components/common/MenuProvider'
+import AlgoliaSearch from '@/components/molecules/AlgoliaSearch'
+import Navigation from '@/components/molecules/Navigation'
+import Link from 'next/link'
+import {useContext} from 'react'
+import styles from './Header.module.css'
+
+// TODO: Create Storybook for this component.
+
+/**
+ * Render the ImageGallery component.
+ *
+ * @author WebDevStudios
+ * @return {Element} The Header component.
+ */
+export default function Header() {
+  const {menus} = useContext(MenuContext)
+  return (
+    <>
+      <a className={styles.skip} href="#page-content">
+        Skip to Main Content
+      </a>
+      <header className={styles.header}>
+        <Container paddingTop={false} paddingBtm={false}>
+          <div className={styles.search}>
+            <AlgoliaSearch
+              className={styles.input}
+              useHistory={true}
+              usePlaceholder={true}
+            />
+          </div>
+          <div className={styles.navigation}>
+            <Link href="/">
+              <a>
+                <Logo className={styles.logo} type="dark" />
+              </a>
+            </Link>
+            <Navigation
+              menu={menus?.primary_menu}
+              styles={styles}
+              className={styles.primaryMenu}
+            />
+          </div>
+        </Container>
+      </header>
+    </>
+  )
+}
